4c0e36495f701339a25d5c5ff7fa811fa37ebf10
Guido <caffaguido@gmail.com>
2021-08-20 14:41:36 -0300

    Quito validacion IP

--- install_insight.sh
+++ install_insight.sh
@@ -19,100 +19,95 @@ then
         hostname -I
         echo "=============================="
         read -p "Enter Server IP or Domain Name: " IP_ADDRESS
-        if ipcalc -cs $IP_ADDRESS
+
+        echo "================================="
+        echo "Please Validate the Information"
+        echo "================================="
+
+        echo "Server IP/Domain: " $IP_ADDRESS
+
+        read -p "Install InvGate Insight ? (no/YES): " INSTALL_RES
+
+        case ${INSTALL_RES,,} in 
+                y|S|si|ye|yes ) INSTALL=true;;
+                n|no|o ) INSTALL=false;;
+                * ) INSTALL=true;;
+        esac
+
+        if [[ "$INSTALL" == "false" ]]
         then
-                echo "================================="
-                echo "Please Validate the Information"
-                echo "================================="
+                echo "Aborting Installation"
+                exit 2
+        fi
 
-                echo "Server IP/DNS: " $IP_ADDRESS
+        echo "[1] Installing postgreesql"
+        yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm >> "$LOG_PATH"
+        echo "[1] Finished"
 
-                read -p "Install InvGate Insight ? (no/YES): " INSTALL_RES
+        echo "[2] Adding EPEL repo"
+        yum install epel-release -y >> "$LOG_PATH"
+        echo "[2] Finished"
 
-                case ${INSTALL_RES,,} in 
-                        y|S|si|ye|yes ) INSTALL=true;;
-                        n|no|o ) INSTALL=false;;
-                        * ) INSTALL=true;;
-                esac
+        echo "[3] Adding CentOS repo"
+        yum install centos-release-scl -y >> "$LOG_PATH"
+        echo "[3] Finished"
 
-                if [[ "$INSTALL" == "false" ]]
-                then
-                        echo "Aborting Installation"
-                        exit 2
-                fi
+        echo "[4] Starting firewalld"
+        service firewalld start >> "$LOG_PATH"
+        echo "[4] Finished"
 
-                echo "[1] Installing postgreesql"
-                yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm >> "$LOG_PATH"
-                echo "[1] Finished"
+        echo "[5] Adding port (80/TCP)"
+        firewall-cmd --permanent --add-port=80/tcp >> "$LOG_PATH"
+        echo "[5] Finished"
 
-                echo "[2] Adding EPEL repo"
-                yum install epel-release -y >> "$LOG_PATH"
-                echo "[2] Finished"
+        echo "[6] Adding port (443/TCP)"
+        firewall-cmd --permanent --add-port=443/tcp >> "$LOG_PATH"
+        echo "[6] Finished"
 
-                echo "[3] Adding CentOS repo"
-                yum install centos-release-scl -y >> "$LOG_PATH"
-                echo "[3] Finished"
+        echo "[7] Reloading firewalld"
+        firewall-cmd --reload >> "$LOG_PATH"
+        echo "[7] Finished"
 
-                echo "[4] Starting firewalld"
-                service firewalld start >> "$LOG_PATH"
-                echo "[4] Finished"
+        echo "[8] Setting enforce"
+        setenforce 0 && sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config >> "$LOG_PATH"
+        echo "[8] Finished"
 
-                echo "[5] Adding port (80/TCP)"
-                firewall-cmd --permanent --add-port=80/tcp >> "$LOG_PATH"
-                echo "[5] Finished"
-
-                echo "[6] Adding port (443/TCP)"
-                firewall-cmd --permanent --add-port=443/tcp >> "$LOG_PATH"
-                echo "[6] Finished"
-
-                echo "[7] Reloading firewalld"
-                firewall-cmd --reload >> "$LOG_PATH"
-                echo "[7] Finished"
-
-                echo "[8] Setting enforce"
-                setenforce 0 && sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config >> "$LOG_PATH"
-                echo "[8] Finished"
-
-                echo "[9] Adding InvGate repo"
-                touch /etc/yum.repos.d/invgate.repo >> "$LOG_PATH"
-                tee -a /etc/yum.repos.d/invgate.repo <<EOL
+        echo "[9] Adding InvGate repo"
+        touch /etc/yum.repos.d/invgate.repo >> "$LOG_PATH"
+        tee -a /etc/yum.repos.d/invgate.repo <<EOL
 [invgate]
 name=InvGate Packages
 baseurl=https://download.invgate.net/neoassets/packages/centos7/
 enabled=1
 gpgcheck=0
 EOL
-                echo "[9] Finished"
+        echo "[9] Finished"
 
-                echo "[10] Installing InvGate Insight"
-                yum install invgate-neo-assets -y >> "$LOG_PATH"
-                if [ "$?" != 0 ]
-                then
-                        echo "[X] Instalation FAILED"
-                        exit 2
-                fi
-                echo "[10] Finished"
-                
-                echo "[11] Configuring InvGate Insight"
-                sed -i "s|ASSETS_DOMAIN.*|ASSETS_DOMAIN=http://$IP_ADDRESS:80|g" /usr/share/invgate/neoassets/neo-assets/.env >> "$LOG_PATH"
-                sed -i "s|NEO_ASSETS_STATIC_URL.*|NEO_ASSETS_STATIC_URL=http://$IP_ADDRESS/static-front|g" /usr/share/invgate/neoassets/neo-assets/.env >> "$LOG_PATH"
-                echo "[11] Finished"
-
-
-                echo "[12] Restarting Apache"
-                systemctl restart httpd24-httpd >> "$LOG_PATH"
-                echo "[12] Finished"
-
-
-                echo "==============================" 
-                more /var/log/httpd/neo-assets-install.log | grep 'Your superadmin'
-                echo "=============================="
-
-                echo "[#] Instalation finished"
-        else
-        echo "Please provide a valid IP Address."
-        exit 1
+        echo "[10] Installing InvGate Insight"
+        yum install invgate-neo-assets -y >> "$LOG_PATH"
+        if [ "$?" != 0 ]
+        then
+                echo "[X] Instalation FAILED"
+                exit 2
         fi
+        echo "[10] Finished"
+        
+        echo "[11] Configuring InvGate Insight"
+        sed -i "s|ASSETS_DOMAIN.*|ASSETS_DOMAIN=http://$IP_ADDRESS:80|g" /usr/share/invgate/neoassets/neo-assets/.env >> "$LOG_PATH"
+        sed -i "s|NEO_ASSETS_STATIC_URL.*|NEO_ASSETS_STATIC_URL=http://$IP_ADDRESS/static-front|g" /usr/share/invgate/neoassets/neo-assets/.env >> "$LOG_PATH"
+        echo "[11] Finished"
+
+
+        echo "[12] Restarting Apache"
+        systemctl restart httpd24-httpd >> "$LOG_PATH"
+        echo "[12] Finished"
+
+
+        echo "==============================" 
+        more /var/log/httpd/neo-assets-install.log | grep 'Your superadmin'
+        echo "=============================="
+
+        echo "[#] Instalation finished"
 else
         echo "The Insight Server must be installed in a Centos/RedHat Linux"
         exit 1
