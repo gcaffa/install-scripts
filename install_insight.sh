79c0204213a938f0434040de2af06ff57ff8db1c
Guido <caffaguido@gmail.com>
2022-01-10 10:22:46 -0300

    Validación de licencia
    - Se agrega la opción para especificar el path de la licencia
    - Se activa la validación de licencias al instalar
    - Se corrigen unos errores con respecto al log

--- install_insight.sh
+++ install_insight.sh
@@ -13,18 +13,23 @@ then
         LOG_FILE_NAME="insight_install_$(date +"%Y_%m_%d_%I_%M").log"
         LOG_PATH="/var/log/insight/$LOG_FILE_NAME"
         mkdir -p /var/log/insight
-        touch LOG_PATH
         echo "=============================="
         echo "Server IPs:"
         hostname -I
         echo "=============================="
-        read -p "Enter Server IP or Domain Name: " IP_ADDRESS
+        read -p "Enter server IP or Domain Name: " IP_ADDRESS
+
+        echo "=============================="
+        echo "Enter full path to insight license file"
+        echo "=============================="
+        read -p "License path: " LICENSE_PATH
 
         echo "================================="
-        echo "Please Validate the Information"
+        echo "Please validate the Information"
         echo "================================="
 
         echo "Server IP/Domain: " $IP_ADDRESS
+        echo "License file: " $LICENSE_PATH
 
         read -p "Install InvGate Insight ? (no/YES): " INSTALL_RES
 
@@ -41,39 +46,39 @@ then
         fi
 
         echo "[1] Installing postgreesql"
-        yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm >> "$LOG_PATH"
+        yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm >> $LOG_PATH
         echo "[1] Finished"
 
         echo "[2] Adding EPEL repo"
-        yum install epel-release -y >> "$LOG_PATH"
+        yum install epel-release -y >> $LOG_PATH
         echo "[2] Finished"
 
         echo "[3] Adding CentOS repo"
-        yum install centos-release-scl -y >> "$LOG_PATH"
+        yum install centos-release-scl -y >> $LOG_PATH
         echo "[3] Finished"
 
         echo "[4] Starting firewalld"
-        service firewalld start >> "$LOG_PATH"
+        service firewalld start >> $LOG_PATH
         echo "[4] Finished"
 
         echo "[5] Adding port (80/TCP)"
-        firewall-cmd --permanent --add-port=80/tcp >> "$LOG_PATH"
+        firewall-cmd --permanent --add-port=80/tcp >> $LOG_PATH
         echo "[5] Finished"
 
         echo "[6] Adding port (443/TCP)"
-        firewall-cmd --permanent --add-port=443/tcp >> "$LOG_PATH"
+        firewall-cmd --permanent --add-port=443/tcp >> $LOG_PATH
         echo "[6] Finished"
 
         echo "[7] Reloading firewalld"
-        firewall-cmd --reload >> "$LOG_PATH"
+        firewall-cmd --reload >> $LOG_PATH
         echo "[7] Finished"
 
         echo "[8] Setting enforce"
-        setenforce 0 && sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config >> "$LOG_PATH"
+        setenforce 0 && sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config >> $LOG_PATH
         echo "[8] Finished"
 
         echo "[9] Adding InvGate repo"
-        touch /etc/yum.repos.d/invgate.repo >> "$LOG_PATH"
+        touch /etc/yum.repos.d/invgate.repo >> $LOG_PATH
         tee -a /etc/yum.repos.d/invgate.repo <<EOL
 [invgate]
 name=InvGate Packages
@@ -84,7 +89,7 @@ EOL
         echo "[9] Finished"
 
         echo "[10] Installing InvGate Insight"
-        yum install invgate-neo-assets -y >> "$LOG_PATH"
+        yum install invgate-neo-assets -y >> $LOG_PATH
         if [ "$?" != 0 ]
         then
                 echo "[X] Instalation FAILED"
@@ -93,14 +98,20 @@ EOL
         echo "[10] Finished"
         
         echo "[11] Configuring InvGate Insight"
-        sed -i "s|ASSETS_DOMAIN.*|ASSETS_DOMAIN=http://$IP_ADDRESS:80|g" /usr/share/invgate/neoassets/neo-assets/.env >> "$LOG_PATH"
-        sed -i "s|NEO_ASSETS_STATIC_URL.*|NEO_ASSETS_STATIC_URL=http://$IP_ADDRESS/static-front|g" /usr/share/invgate/neoassets/neo-assets/.env >> "$LOG_PATH"
+        sed -i "s|ASSETS_DOMAIN.*|ASSETS_DOMAIN=http://$IP_ADDRESS:80|g" /usr/share/invgate/neoassets/neo-assets/.env >> $LOG_PATH
+        sed -i "s|NEO_ASSETS_STATIC_URL.*|NEO_ASSETS_STATIC_URL=http://$IP_ADDRESS/static-front|g" /usr/share/invgate/neoassets/neo-assets/.env >> $LOG_PATH
         echo "[11] Finished"
 
-
-        echo "[12] Restarting Apache"
-        systemctl restart httpd24-httpd >> "$LOG_PATH"
+        echo "[12] Configuring Insight license"
+        cp $LICENSE_PATH /usr/share/invgate/neoassets/neo-assets/invgate.pato
+        source /usr/share/invgate/neoassets/neo-assets/.venv/bin/activate >> $LOG_PATH
+        python /usr/share/invgate/neoassets/neo-assets/manage.py enable_licence_check_feature_flag --settings=assets.settings.production >> $LOG_PATH
         echo "[12] Finished"
+
+
+        echo "[13] Restarting Apache"
+        systemctl restart httpd24-httpd >> $LOG_PATH
+        echo "[13] Finished"
 
 
         echo "==============================" 
